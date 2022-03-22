56edd36018f80c6d4799ab0308beca8acd4f7e5e
Shashikanth Reddy <kolanshashii@gmail.com>
2022-03-22 11:56:08 +0530

    added about section template

---
+++ components/About.js
@@ -0,0 +1,31 @@
+const About = () => {
+  return (
+    <div className="w-full bg-blue">
+      <div className="max-w-else w-full mx-auto px-8 pt-16 pb-60 text-white flex">
+        <div className="w-full mr-8">
+          <div className="mb-8 text-6xl font-bold">About.</div>
+          <div className="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo illo
+            rem dolorum et cum, totam mollitia accusantium debitis itaque harum
+            consequuntur voluptas eligendi officia quos nihil doloremque
+            incidunt? Fuga asperiores consectetur quam praesentium corrupti
+            veniam omnis corporis adipisci ipsa accusantium, unde qui sunt esse
+            earum ab ratione perferendis laborum consequatur!
+          </div>
+        </div>
+        <div className="w-full text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat esse
+          asperiores, laudantium quos, inventore explicabo reprehenderit omnis
+          reiciendis sed quaerat error, magnam adipisci nesciunt iure modi velit
+          nostrum voluptatibus officiis aut dolorem sequi? Sed quaerat possimus
+          neque exercitationem dignissimos aperiam ipsum recusandae natus et
+          molestias quis voluptatem deleniti omnis laboriosam, quisquam, optio
+          expedita aut explicabo quasi harum ad doloribus. Necessitatibus
+          facilis placeat, cupiditate numquam velit sequi perspiciatis, eum id
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
