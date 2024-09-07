44029f7809ae263e65a21640704f739bb279ef62
Shashikanth Reddy <kolanshashii@gmail.com>
2024-09-07 19:06:07 -0400

    feat: Update resume link

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,7 +3,7 @@ const NavBar = () => {
     <div className="flex justify-between items-center w-full h-16 small-phone:h-24">
       <div className="triangle"></div>
       <a
-        href="https://drive.google.com/file/d/1U1mwThFrgehxXRzR2iBgbtd5fGZMBmDs/view"
+        href="https://drive.google.com/file/d/1PRvagIEgWm1Avys7zvqkVhC8yqGsXVFV/view"
         target="_blank"
         rel="noopener noreferrer"
         className="bg-yellow mr-4 small-phone:mr-8 px-6 py-2 font-bold text-inherit"
